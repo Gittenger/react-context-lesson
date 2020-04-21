9ff3db1e955dafd381e2700d734abf1c2de72a11
John Pittenger <john.pittenger86@gmail.com>
2020-04-21 00:39:06 -0400

    p2 cart conversion to context

--- src/providers/cart.provider.jsx
+++ src/providers/cart.provider.jsx
@@ -1,25 +1,43 @@
 import React, { useState, createContext, useEffect } from "react";
 
-import { addItemToCart, removeItemFromCart } from "./cart.utils.js";
+import {
+  addItemToCart,
+  removeItemFromCart,
+  filterItemFromCart,
+  getCartItemsCount,
+  getCartItemsTotal,
+} from "./cart.utils.js";
 
 export const CartContext = createContext({
   hidden: true,
   toggleHidden: () => {},
   cartItems: [],
   cartItemsCount: 0,
+  cartItemsTotal: 0,
   addItem: () => {},
   removeItem: () => {},
   clearItemFromCart: () => {},
 });
 
 const CartProvider = ({ children }) => {
-  //use hooks to set default states and define local state to pass into context consumers
+  //use hooks to set local state values to pass into context consumers
   const [hidden, setHidden] = useState(true);
   const [cartItems, setCartItems] = useState([]);
   const [cartItemsCount, setCartItemsCount] = useState(0);
+  const [cartItemsTotal, setCartItemsTotal] = useState(0);
 
+  //define setter functions
   const addItem = (item) => setCartItems(addItemToCart(cartItems, item));
+  const removeItem = (item) =>
+    setCartItems(removeItemFromCart(cartItems, item));
+  const clearItemFromCart = (item) =>
+    setCartItems(filterItemFromCart(cartItems, item));
   const toggleHidden = () => setHidden(!hidden);
+
+  useEffect(() => {
+    setCartItemsCount(getCartItemsCount(cartItems));
+    setCartItemsTotal(getCartItemsTotal(cartItems));
+  }, [cartItems]);
 
   return (
     <CartContext.Provider
@@ -28,7 +46,10 @@ const CartProvider = ({ children }) => {
         toggleHidden,
         cartItems,
         cartItemsCount,
+        cartItemsTotal,
         addItem,
+        removeItem,
+        clearItemFromCart,
       }}
     >
       {children}
--- src/providers/cart.utils.js
+++ src/providers/cart.utils.js
@@ -1,10 +1,10 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
-    cartItem => cartItem.id === cartItemToAdd.id
+    (cartItem) => cartItem.id === cartItemToAdd.id
   );
 
   if (existingCartItem) {
-    return cartItems.map(cartItem =>
+    return cartItems.map((cartItem) =>
       cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
@@ -16,16 +16,28 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const existingCartItem = cartItems.find(
-    cartItem => cartItem.id === cartItemToRemove.id
+    (cartItem) => cartItem.id === cartItemToRemove.id
   );
 
   if (existingCartItem.quantity === 1) {
-    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
   }
 
-  return cartItems.map(cartItem =>
+  return cartItems.map((cartItem) =>
     cartItem.id === cartItemToRemove.id
       ? { ...cartItem, quantity: cartItem.quantity - 1 }
       : cartItem
   );
 };
+
+export const filterItemFromCart = (cartItems, cartItemToClear) =>
+  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
+export const getCartItemsCount = (cartItems) =>
+  cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0);
+
+export const getCartItemsTotal = (cartItems) =>
+  cartItems.reduce(
+    (acc, cartItem) => acc + cartItem.quantity * cartItem.price,
+    0
+  );
